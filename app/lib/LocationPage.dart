0a5bd62e5e5f5947aed6c927cb323a8b78eb7db5
unknown <anna.m.george@outlook.com>
2020-03-10 13:12:25 -0500

    added audio button to location page

--- app/lib/LocationPage.dart
+++ app/lib/LocationPage.dart
@@ -1,28 +1,13 @@
-import 'package:audioplayers/audio_cache.dart';
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-//import './TextSection.dart';
-//import './TitleSection.dart';
-//import './AboutImage.dart';
+import './TextSection.dart';
+import './TitleSection.dart';
+import './AboutImage.dart';
+import './Audio.dart';
 
 class LocationPage extends StatelessWidget {
-
-  static AudioPlayer audioPlayer = AudioPlayer();
-
-  static int play = 0;
-
-  void playAudio() async {
-    audioPlayer = await AudioCache().play("songExample.mp3");
-  }
-
-  void stopAudio() async {
-    await audioPlayer.pause();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: new AppBar(title: Text("Location"),
         actions: <Widget>[ 
           IconButton(
@@ -40,30 +25,15 @@ class LocationPage extends StatelessWidget {
         ],
       ),
       body: Center(
-        
         child: ListView(
           children: <Widget>[
-            IconButton(
-            icon: Icon(
-              Icons.bookmark,
-              color: Colors.black,
-            ),
-            onPressed: (){
-              if(play % 2 == 0){
-                playAudio();
-                play++;
-              } else {
-                stopAudio();
-              }
-            },
-          ),
-            //TitleSection(),
-            //AboutImage(),
-            //TextSection(),
-            //TextSection(),
-            //TextSection(),
+            TitleSection(),
+            AboutImage(),
+            TextSection(),
+            TextSection(),
+            TextSection(),
+            Audio(),
           ],
-          
         ), 
       ),
     );
