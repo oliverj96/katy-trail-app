1806474e6d1fbb0c2c79f3b7fa2ef70087a78280
oliverj96 <ojohnson.com@gmail.com>
2020-03-12 15:16:23 -0500

    Cleaned up some code/comments

--- app/lib/map_page/maps.dart
+++ app/lib/map_page/maps.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
-import 'dart:io';
-import 'dart:async';
 import 'package:flutter/services.dart' show rootBundle;
-// import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
 class MapPage extends StatefulWidget {
   MapPage({Key key}) : super(key: key);
@@ -22,10 +20,8 @@ class _MapPageState extends State<MapPage> {
     return await data.then((dataPoints) {
       var dump = dataPoints.split(' ');
       for (var i = 0; i < dump.length - 2; i += 2) {
-        // print(i);
         var newPoint =
             new LatLng(double.parse(dump[i]), double.parse(dump[i + 1]));
-        // print(newPoint);
         points.add(newPoint);
       }
     });
@@ -33,22 +29,21 @@ class _MapPageState extends State<MapPage> {
 
   final sampleData = [
     {"name": "Location 1", "long": 38.766964, "lat": -90.489257},
-    // {"name": "Location 2", "long": 12.1, "lat": 12.1},
   ];
 
-  final points = <LatLng>[
-    // new LatLng(38.766964, -90.489257),
-    // new LatLng(38.800099, -90.470506),
-  ];
+  final points = <LatLng>[];
 
   @override
   Widget build(BuildContext context) {
-    // TODO read from file
-
+    // Build map path from file
     var data = loadAsset('assets/docs/path.txt');
     getPoints(data);
+
+    // Dynamically add markers to List
+    // TODO Once firebase is integrated, change sample data to pulled data
     var locationPlaces = List<Marker>();
     for (var location in sampleData) {
+      // Create marker widget for each location
       var temp = new Marker(
           width: 45.0,
           height: 45.0,
@@ -59,28 +54,28 @@ class _MapPageState extends State<MapPage> {
                   color: Colors.red,
                   iconSize: 45.0,
                   onPressed: () {
+                    // TODO Add card once tapped
                     print("Location: " + location["name"] + " was tapped.");
                   },
                 ),
               ));
-          locationPlaces.add(temp);
+      // Append location to list of places
+      locationPlaces.add(temp);
     }
-    // setState(() {
-    // });
-    // dump.add(15);
-    // String data = await getFileData('assets/docs/path.txt');
 
-    // var test = pathFile.readAsString();
-    // test.then((String contents){
-    //   print(contents);
-    // });
+    // Retrieve API url and token
+    // TODO Retrieve from Key files from the asset folder
     String url, token;
     url =
         'https://api.mapbox.com/styles/v1/ojohnson7cc/ck79a877u2ffj1jnn4dfgh3r9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib2pvaG5zb243Y2MiLCJhIjoiY2s3OWE0ZG5nMHIyaDNlcWh4cHd5N3I2bSJ9.L1xfay1JISdfIO1jDp8rTg';
     token =
         'sk.eyJ1Ijoib2pvaG5zb243Y2MiLCJhIjoiY2s3OWp2cnNqMHUydzNlcWtxd2R4c2JncCJ9.keCK6gFmt7EO9Ug4GwC_jg';
+    
+    // TODO Get location and map onto the map
     // var geolocator = Geolocator();
     // var locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
+    
+    // Create Flutter Map Widget
     return FlutterMap(
       options: new MapOptions(
           center: new LatLng(38.77699, -90.482418), minZoom: 10.0),
@@ -96,9 +91,7 @@ class _MapPageState extends State<MapPage> {
             color: Colors.blue,
           )
         ]),
-        new MarkerLayerOptions(markers: 
-          locationPlaces
-        ),
+        new MarkerLayerOptions(markers: locationPlaces),
       ],
     );
   }
