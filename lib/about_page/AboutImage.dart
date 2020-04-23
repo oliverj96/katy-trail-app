70fefde2d947a516f9c34104f13839882c1ba8b9
Shraddha <shraddha.belbase1@gmail.com>
2020-04-23 17:05:37 -0500

    Descriptions

--- lib/about_page/AboutImage.dart
+++ lib/about_page/AboutImage.dart
@@ -6,7 +6,7 @@ class AboutImage extends StatelessWidget {
     return Center(
       child: Container(
         margin: EdgeInsets.only(right: 30, left: 30, bottom: 15),
-        child: Image.asset('assets/images/katy_trail.jpg'),
+        child: Image.asset('assets/images/katy_trail.png'),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -58,11 +58,59 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   static var sampleData = [
-    {"name": "Missouri’s first capitol", "description": "Missouri's capitol from 1821-1826", "long": 38.780073, "lat": -90.481848},
-    {"name": "Masonic Hall 1849", "description": "Used for local Masons Lodge to meet", "long": 38.780621, "lat": -90.481401},
-    {"name": "Daniel Boone Burial Site", "description": "Monument and burial site of Daniel Boone", "long": 38.621592, "lat": -91.034315},
-    {"name": "Peers Store", "description": "Historic general store for travelers", "long": 38.634266, "lat": -91.122865},
-    {"name": "Daniel Boone Judgment Tree", "description": "Served as Boone's courthouse", "long": 38.608879, "lat": -90.794029},
+    {
+    "name": "Missouri’s First Capitol",
+    "description": "Missouri's first capital building.",
+    "LongDes": "The first Missouri State Capitol State Historic Site holds years of history."+
+                "It was Missouri’s first capitol building between statehood in 1821 and the capitol moving to Jefferson City in 1826."+
+                "The building was restored in the 1960s. It features 11 rooms to take you back in history."+
+                "Tours available at the State Capitol Historic Site office next door."+
+                "Here’s the site today, and before the state restored it."+
+                "(Images: St. Charles County Historical Society, Missouri State Parks)",
+    "long": 38.784926, 
+    "lat": -90.478854,
+    "images" : "FCapitol1.png" "FCapitol2.png"
+   
+
+    },
+
+    {
+    "name": "Pilot Groove", 
+    "description": "Small horse-powered mill settlement", 
+    "LongDes": "Little is known about the origin of Pilot Grove,"+
+                "although a small horse-powered mill settlement was created in 1820 on a tributary of the Petite Saline Creek."+
+                "Half a mile northeast of Pilot Grove a Katy Railroad train carrying WWII supplies of crude oil and artillery shells"+
+                "derailed and exploded, sending shrapnel a half mile down to the Pilot Grove depot",
+    "long": 38.60881, 
+    "lat": -90.794006,
+    "images" : "PilotGroove1.png" "PilotGroove2.png"
+    },
+
+    {
+      "name": "Augusta Cemetery",
+     "description": "Monument and burial site of Daniel Boone",
+     "LongDes": "Just a mile’s ride north of here is the Augusta Cemetery, created in 1859."+
+                "The earliest residents of Augusta were German immigrants, attracted here by its landscape—and"+
+                "the writings of Gottfried Duden, promoting it a “Rhineland.” When you arrive at the cemetery,"+
+                 "the oldest sections are to the south (your right as you face the gate)." +
+                 "Notice the inscriptions in German that date to even the early twentieth century, like this one of John Fuhr.", 
+     "long": 38.580103, 
+     "lat": -90.884011,
+     "images" : "Augusta.png"
+     },
+
+    {
+      "name": "MKT RAILROAD ",
+     "description": "The Missouri Kansas & Texas Railroad",
+     "LongDes": "The Missouri Kansas & Texas Railroad—the “Katy”—was built in 1865;"+
+                "; Union Pacific acquired it in 1988. The Katy Railroad had more than"+
+                "3,377 miles of track  servicing a booming coal industry."+
+                "Engines like this one connected the Midwest to Texas.",
+      "long": 38.777167, 
+      "lat": -90.482046,
+      "images" : "MKT.png"
+     },
+
   ];
 
   Widget currentScreen = HomePage(sampleData, points, bmHandler);
@@ -75,3 +123,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
  } //uncomment after showing push notifications example
 }
+
