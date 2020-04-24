95d4b08b189f2510b12de6cec44583359a363a12
oliverj96 <ojohnson.com@gmail.com>
2020-04-24 17:44:07 -0500

    started fixing

--- lib/push_handler.dart
+++ lib/push_handler.dart
@@ -9,7 +9,6 @@ class PushHandler {
   Map<String, Object> _locationData;
   PushHandler(this.dataPointsCol);
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
-  
 
   FlutterLocalNotificationsPlugin get localNotificationPlugin {
     return flutterLocalNotificationsPlugin;
@@ -22,15 +21,16 @@ class PushHandler {
   // Get the user's longitude and latitude positions.
   void getPosition() async {
     try {
-      Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      _longitude = double.parse(position.latitude.toString());
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      _longitude = double.parse(position.longitude.toString());
       _latitude = double.parse(position.latitude.toString());
-    } catch (e) { 
+    } catch (e) {
       // Ignore errors.
     }
   }
 
-    showNotification(Map<String, Object> locDetails) async {
+  showNotification(Map<String, Object> locDetails) async {
     var android = new AndroidNotificationDetails(
         'channel id', 'channel NAME', 'CHANNEL DESCRIPTION',
         priority: Priority.High, importance: Importance.Max);
@@ -40,31 +40,26 @@ class PushHandler {
         0, locDetails['name'], 'Learn about this area!', platform);
   }
 
-
   // Use to check if a user's current location is near a location's vicinity
   checkDistance() {
-    var distance = 0.006;
+    const distance = 0.01;
     var latDistance = 0.0;
     var longDistance = 0.0;
-    print(_longitude);
     for (var location in dataPointsCol) {
       // Go through all the points in the database coming in ["lat"] ["long"]
 
       latDistance = (_latitude - location["lat"]).abs();
       longDistance = (_longitude - location["long"]).abs();
 
+      var nearBy = (latDistance <= distance) && (longDistance <= distance);
       // If user is close enough to a location but has not previously been close enough to that location
-      if ((latDistance <= distance) &&
-          (longDistance <= distance) &&
-          !isUserNearLocation) {
-        isUserNearLocation = false;
+      if (nearBy && !isUserNearLocation) {
+        isUserNearLocation = true;
         _locationData = location;
         showNotification(location); // Send the push notification
       }
       // If user is close enough to a location and continues to be close enough to that location
-      else if ((latDistance <= distance) &&
-          (longDistance <= distance) &&
-          isUserNearLocation) {
+      else {
         isUserNearLocation = true;
       }
     }
