81226a2bac7944b9cd978bd22aab4445cec4117e
unknown <anna.m.george@outlook.com>
2020-04-29 10:46:10 -0500

    added bookmark icon to about page

--- lib/about_page/about_page.dart
+++ lib/about_page/about_page.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:katy_trail_app/bookmark_page/bm_handler.dart';
+import 'package:katy_trail_app/bookmark_page/bookmarks.dart';
 import './text_section.dart';
 import './title_section.dart';
 import './about_image.dart';
 
 class AboutPage extends StatelessWidget {
+  final BookmarkHandler bmHandler;
+  AboutPage(this.bmHandler); 
+
   @override
   Widget build(BuildContext context) {
   return Scaffold(
-    appBar: AppBar(title: Text("About")),
+    appBar: AppBar(title: Text("About"),
+      actions: <Widget>[ 
+        IconButton(
+          icon: Icon(
+            Icons.bookmark,
+            color: Colors.white,
+          ),
+          onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => new Bookmarks(bmHandler)),
+            );
+          },
+        ),
+      ],
+    ),
       body: Center(
         child: ListView(
           children: <Widget>[
--- lib/home_page/about.dart
+++ lib/home_page/about.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:katy_trail_app/bookmark_page/bm_handler.dart';
 import '../about_page/about_page.dart';
 
 class About extends StatelessWidget {
-  const About({Key key}) : super(key: key);
+
+  final BookmarkHandler bmHandler;
+  About(this.bmHandler); 
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +18,7 @@ class About extends StatelessWidget {
               contentPadding: EdgeInsets.symmetric(vertical: 15.0),
               onTap: (){
                 Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => AboutPage()),
+                  MaterialPageRoute(builder: (context) => AboutPage(bmHandler)),
                 );
               },
               leading: Image.asset('assets/images/about.png', height: 100, width: 100),
